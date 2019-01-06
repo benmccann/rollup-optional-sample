1840afb8873efd6f0c3fccd56f8a0276d47fdee0
Ben McCann <322311+benmccann@users.noreply.github.com>
2019-01-05 21:52:17 -0800

    Add minimized builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 /* eslint-env es6 */
 
 const resolve = require('rollup-plugin-node-resolve');
+const terser = require('rollup-plugin-terser').terser;
 
 const input = 'src/main.js';
 
 module.exports = [
 	// UMD builds (excluding moment)
+	// dist/main.min.js
 	// dist/main.js
 	{
 		input: input,
@@ -24,8 +26,28 @@ module.exports = [
 			'luxon'
 		]
 	},
+	{
+		input: input,
+		plugins: [
+			resolve(),
+			terser()
+		],
+		output: {
+			name: 'main',
+			file: 'dist/main.min.js',
+			format: 'umd',
+			indent: false,
+			globals: {
+				luxon: 'luxon'
+			}
+		},
+		external: [
+			'luxon'
+		]
+	},
 
-	// UMD builds (including moment)
+	// UMD builds (including moment/luxon)
+	// dist/main.bundle.min.js
 	// dist/main.bundle.js
 	{
 		input: input,
@@ -36,6 +58,19 @@ module.exports = [
 			name: 'main',
 			file: 'dist/main.bundle.js',
 			format: 'umd'
+		}
+	},
+	{
+		input: input,
+		plugins: [
+			resolve(),
+			terser()
+		],
+		output: {
+			name: 'main',
+			file: 'dist/main.bundle.min.js',
+			format: 'umd',
+			indent: false
 		}
 	}
 ];
